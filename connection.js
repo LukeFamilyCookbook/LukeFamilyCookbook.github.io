bb62eb324ab50cc6ae88ddc53d9428169f245326
LukeFamilyCookbook <46092324+LukeFamilyCookbook@users.noreply.github.com>
2018-12-23 14:57:00 -0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,5 +1,6 @@
 function init() {
- Tabletop.init( { key: ‘1rDixZ4hdeQMMJt401cXYPGsCfCN3VMW6v4sbhsz2NpM',
+ Tabletop.init( { key: 
+ ‘https://docs.google.com/spreadsheets/d/1rDixZ4hdeQMMJt401cXYPGsCfCN3VMW6v4sbhsz2NpM',
  callback: function(data, tabletop) { 
  console.log(data)
  },
