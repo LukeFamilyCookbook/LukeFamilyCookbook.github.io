502be1cf710ba849c8da5cd32ce86ba1eadb31fb
LukeFamilyCookbook <46092324+LukeFamilyCookbook@users.noreply.github.com>
2018-12-23 14:55:12 -0500

    Create connection.js

---
+++ connection.js
@@ -0,0 +1,8 @@
+function init() {
+ Tabletop.init( { key: ‘1rDixZ4hdeQMMJt401cXYPGsCfCN3VMW6v4sbhsz2NpM',
+ callback: function(data, tabletop) { 
+ console.log(data)
+ },
+ simpleSheet: true } )
+}
+window.addEventListener(‘DOMContentLoaded’, init)
